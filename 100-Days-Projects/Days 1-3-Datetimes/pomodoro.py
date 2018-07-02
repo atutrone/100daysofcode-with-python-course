6ef6d64fdf4837478b2d07972c103a3f12ee0907
Anthony Tutrone <anthony@tutrone.net>
2018-07-02 09:35:13 -0400

    Added 5 minute and 25 minute timers

--- 100-Days-Projects/Days 1-3-Datetimes/pomodoro.py
+++ 100-Days-Projects/Days 1-3-Datetimes/pomodoro.py
@@ -8,22 +8,35 @@ def countdown(t):
         timeformat = '{:02d}:{:02d}'.format(mins, secs)
         print(timeformat, end='\r')
         time.sleep(60)
+        #time.sleep(1) #for testing
         t -= 1
-
+count = 0
 now = datetime.now()
-nowPlusOne = now + timedelta(minutes = 25)
+nowPlus25 = now + timedelta(minutes = 25)
 
 nowMinute = now.minute
-nowPlusOneMin = nowPlusOne.minute
+nowPlus25Min = nowPlus25.minute
 
-while (nowMinute != nowPlusOneMin):
+while count <= 4:
+    while (nowMinute != nowPlus25Min):
+        print("Break time will be at: " + str(nowPlus25))
+        countdown(25)
         time.sleep(1)
         now = datetime.now()
         nowMinute = now.minute
-        print("Break time will be at: " + str(nowPlusOne))
-        countdown(25)
+        # if nowMinute == nowPlus25Min:
+        #     break
 
+    print("Take a break!")
 
+    now = datetime.now()
+    nowPlusFive = now + timedelta(minutes = 5)
+    nowPlusFiveMin = nowPlusFive.minute
 
-print("Take a break!")
-
+    while (nowMinute != nowPlusFiveMin):
+        print("Work timer will start at: " + str(nowPlusFive))
+        countdown(5)
+        time.sleep(1)
+        now = datetime.now()
+        nowMinute = now.minute
+    count += 1
